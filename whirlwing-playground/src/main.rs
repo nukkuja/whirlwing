9eaf54498d47a51b4e385e6a51af9b169142e377
nukkuja <133861188+nukkuja@users.noreply.github.com>
2023-07-27 23:15:24 +0300

    Ended with log for now. Wrote some docs

--- whirlwing-playground/src/main.rs
+++ whirlwing-playground/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    use whirlwing::wwg_log::{err, info, trace, warn, wwg_err, wwg_info, wwg_trace, wwg_warn};
-    whirlwing::wwg_log::log_utils::set_log_default(whirlwing::wwg_log::log_utils::Severity::None);
+    use whirlwing::wwg_log::{
+        err, info,
+        log_utils::{set_log_default, Severity},
+        trace, warn, wwg_err, wwg_info, wwg_trace, wwg_warn,
+    };
+    set_log_default(Severity::Trace);
     let f = 5;
     trace!("I'm calling trace macro! Var = {}.", f);
     info!("This is info macro. This information was provided to you by me :)");
