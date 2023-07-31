c44f6420c1f3067a332b382e4b597ef4908144b8
nukkuja <133861188+nukkuja@users.noreply.github.com>
2023-07-31 14:58:46 +0300

    Creating opengl context for my window window window

--- whirlwing-playground/src/main.rs
+++ whirlwing-playground/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
+    whirlwing::wwg_log::log_utils::set_engine_log_default(whirlwing::wwg_log::log_utils::Severity::Info);
     whirlwing::app::Application::new().run();
 }
--- whirlwing/crates/platform/wwg-windows/src/lib.rs
+++ whirlwing/crates/platform/wwg-windows/src/lib.rs
@@ -1,14 +1,17 @@
 use windows::{
-    core::PCWSTR,
+    core::PCWSTR, core::PCSTR,
     Win32::Foundation::{
         HWND, WPARAM, LPARAM, LRESULT, RECT,
     },
     Win32::{
         UI::WindowsAndMessaging::*,
-        Graphics::Gdi::{HBRUSH, GetDC},
-        System::LibraryLoader::GetModuleHandleW,
+        Graphics::{
+            Gdi::{HBRUSH, GetDC},
+            OpenGL::*,
+        },
+        System::LibraryLoader::{GetModuleHandleW, LoadLibraryW, GetProcAddress},
     },
-    w,
+    w, s,
 };
 
 pub struct WindowInternal {
@@ -78,6 +81,50 @@ pub fn create_window() -> WindowInternal {
 
     unsafe { ShowWindow(hwnd, SW_SHOW); }
 
+    let pfd = PIXELFORMATDESCRIPTOR {
+        nSize: std::mem::size_of::<PIXELFORMATDESCRIPTOR>() as u16,
+        nVersion: 1,
+        dwFlags: PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+        iPixelType: PFD_TYPE_RGBA,
+        cColorBits: 32,
+        cDepthBits: 24,
+        cStencilBits: 8,
+        ..Default::default()
+    };
+
+    unsafe {
+        let hdc = GetDC(hwnd);
+        let format = ChoosePixelFormat(hdc, &pfd);
+        SetPixelFormat(hdc, format, &pfd);
+
+        let hglrc = wglCreateContext(hdc).unwrap();
+        wglMakeCurrent(hdc, hglrc);
+
+        gl::load_with(|string| {
+            let fn_name = PCSTR(format!("{string}\0").as_ptr());
+
+            let hmodule = LoadLibraryW(w!("opengl32.dll")).unwrap();
+
+            match GetProcAddress(hmodule, fn_name) {
+                Some(val) => return val as *const std::os::raw::c_void,
+                None => {
+                    match wglGetProcAddress(fn_name) {
+                        Some(val) => return val as *const std::os::raw::c_void,
+                        None => return std::ptr::null(),
+                    }
+                }
+            }
+        });
+
+        let wglCreateContextAttribsARB = wglGetProcAddress(s!("wglCreateContextAttribsARB")).unwrap();
+    }
+
+    unsafe {
+        gl::Viewport(0, 0, 800, 600);
+        gl::ClearColor(1f32, 0f32, 1f32, 1f32);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+    }
+
     WindowInternal { hwnd }
 }
 
@@ -85,7 +132,7 @@ unsafe extern "system"
 fn window_proc(hwnd: HWND, umsg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     match umsg {
         WM_DESTROY => {
-            wwg_log::warn!("Posting quit message");
+            wwg_log::wwg_info!("Posting quit message");
             PostQuitMessage(0);
             LRESULT::default()
         }
